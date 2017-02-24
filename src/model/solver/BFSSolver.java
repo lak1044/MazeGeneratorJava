c9cf7b3ceacc39d2d9fae667e04a91a773aa59b6
lak1044 <lak1044@rit.edu>
2017-02-24 11:35:33 -0500

    Added BFS solver file to git

---
+++ src/model/solver/BFSSolver.java
@@ -0,0 +1,59 @@
+package model.solver;
+
+import model.Cell;
+import model.MazeModel;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by lak1044 on 2/24/2017.
+ */
+public class BFSSolver extends Solver {
+
+    private Queue<Cell> queue;
+    private boolean completed;
+
+    public BFSSolver(MazeModel mazeModel) {
+        super(mazeModel);
+        this.queue = new LinkedList<>();
+        MazeModel.current.visit();
+        MazeModel.current.setTemporary();
+        ArrayList<Cell> randomNeighbors = this.getRandomAccessibleNeighbors(MazeModel.current);
+        for (Cell neighbor: randomNeighbors) {
+            neighbor.setParent(MazeModel.current);
+            MazeModel.current.setChild(neighbor);
+            queue.add(neighbor);
+        }
+        this.completed = false;
+        System.out.println("DFSSolver created");
+    }
+
+    public void solve() {
+        ArrayList<Cell> randomNeighbors;
+        if (this.completed) {
+            MazeModel.current.setInSolution();
+            MazeModel.current = MazeModel.current.getParent();
+        } else {
+            //1. Dequeue first element in queue and make it current
+            MazeModel.current = queue.remove();
+            MazeModel.current.setTemporary();
+            MazeModel.current.getParent().setTemporary();
+            //2. Get random neighbors, set parents/children, and add them to queue
+            randomNeighbors = this.getRandomAccessibleNeighbors(MazeModel.current);
+            for (Cell neighbor : randomNeighbors) {
+                if (!neighbor.equals(MazeModel.current.getParent())) {
+                    neighbor.setParent(MazeModel.current);
+                    MazeModel.current.setChild(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+            MazeModel.current.setTemporary();
+            if (MazeModel.current.equals(goal)) {
+                this.completed = true;
+            }
+        }
+
+    }
+}
